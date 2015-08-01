1c3ebe21e5279b2abe8446bba3437da69b2d68a3
Pablo Saavedra <psaavedra@devspark.com>
2015-08-01 12:25:13 -0300

    Added type initialization to the API startup

--- util/elasticsearch.js
+++ util/elasticsearch.js
@@ -1,5 +1,7 @@
-/* global process */
+/// <reference path="../typings/lodash/lodash.d.ts"/>
 var elasticsearch = require('elasticsearch');
+var fs = require('bluebird').promisifyAll(require('fs'));
+var _ = require('lodash');
 
 var client = new elasticsearch.Client({
     _index: 'ringo',
@@ -17,6 +19,22 @@ client.indices.create({
         return;
     }
     console.log('Index named ringo successfully created');
+    fs.readFileAsync('./mappings/ringo.json')
+        .then(JSON.parse).then(_.property('mappings'))
+        .then(function (mappings) {
+            return _.map(mappings, function (mapping, type) {
+                console.log('Creating mapping for type', type, 'with value', mapping);
+                return client.indices.putMapping({
+                    index: 'ringo',
+                    type: type,
+                    body: mapping
+                });
+            });
+        }).then(function (result) {
+            console.log('Successfully created', result.length, 'mappings');
+        }).catch(function (error) {
+            console.log('Error creating mappings', error);
+        });
 });
 
 module.exports = client;
