cae6e35436c3f30bd7015461c955ee0db9a5c357
Luciana Zavaleta <lucianazavaleta@gmail.com>
2015-09-05 10:46:46 -0300

    Fixed elasticsearch connection configuraiton, and added retry logic when the connection is not available

--- util/elasticsearch.js
+++ util/elasticsearch.js
@@ -3,22 +3,16 @@ var elasticsearch = require('elasticsearch');
 var fs = require('bluebird').promisifyAll(require('fs'));
 var _ = require('lodash');
 
+var url = process.env.ES_URL || 'localhost:9200';
+console.log('Connecting to elasticsearch on', url);
+
 var client = new elasticsearch.Client({
     _index: 'ringo',
-    server: {
-        host: process.env.ES_URL || 'localhost:9200',
-        log: "trace"
-    }
+    host: url,
+    log: "debug"
 });
 
-client.indices.create({
-    index: 'ringo'
-}, function (err, res) {
-    if (err) {
-        console.log('Index ringo already existed');
-        return;
-    }
-    console.log('Index named ringo successfully created');
+var populate = function () {
     fs.readFileAsync('./mappings/ringo.json')
         .then(JSON.parse).then(_.property('mappings'))
         .then(function (mappings) {
@@ -35,6 +29,28 @@ client.indices.create({
         }).catch(function (error) {
             console.log('Error creating mappings', error);
         });
-});
+}
+
+var interval = setInterval(function () {
+    client.indices.create({
+        index: 'ringo'
+    }, function (err, res) {
+        if (!err) {
+            console.log('Index named ringo successfully created');
+            clearInterval(interval);
+            populate();
+            return;
+        }
+        console.log('Error creating the index', err);
+        if (err.message == 'No Living connections') {
+            console.log('Connection could not be established, retrying');
+            return;
+        }
+        console.log('Index already existed');
+        clearInterval(interval);
+    });
+
+}, 1000);
+
 
 module.exports = client;
